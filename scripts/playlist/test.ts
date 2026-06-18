aa683973db20e880e807fa5ea49055f963d5c66e
Kalnev <vbarishnik50@gmail.com>
2026-06-18 13:57:08 +0300

    Update test.ts

--- scripts/playlist/test.ts
+++ scripts/playlist/test.ts
@@ -27,7 +27,7 @@ const CHANNEL_SPECIFIC_SOURCES: Record<string, string> = {
   'стс' : 'https://raw.githubusercontent.com/Dmitriy11223455/iptv-autoupdate/refs/heads/main/playlist.m3u',
   'матч тв sd' : 'https://drm-play.com/iptv.php?tv=nodrm-1',
   'матч тв' : 'https://raw.githubusercontent.com/Dmitriy11223455/iptv-autoupdate/refs/heads/main/playlist.m3u',
-  'матч! арена' : 'https://drm-play.com/iptv.php?tv=nodrm-1'
+  'матч! арена' : 'https://drm-play.com/iptv.php?tv=nodrm-1',
   'матч! игра' : 'https://drm-play.com/iptv.php?tv=nodrm-1',
   'матч тв hd': 'https://drm-play.com/iptv.php?tv=nodrm-1',
   'матч! премьер hd': 'https://drm-play.com/iptv.php?tv=nodrm-1',
